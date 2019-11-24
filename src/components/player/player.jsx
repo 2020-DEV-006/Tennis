a81445115e5a72965b6f8389530994116bc7b668
2020-DEV-006 <2020dev006@gmail.com>
2019-11-24 23:24:00 +0530

    Handle player first ball click

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 
 export default class Player extends Component {
-    constructor(props){
-        super(props)
-    }
-    updateScore = (score) => {
-        this.props.updateScore(score);
-    };
-    render() {
-        var { playerName, playerNumber } = this.props;
-        return (
-        <div>
-            <div className="playerHeading">{ playerName }</div>
-            <div className="player" value = { playerNumber } defaultValue = { playerNumber  }></div>
-        </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+  }
+  updateScore = score => {
+    this.props.updateScore(score);
+  };
+  render() {
+    var { playerName, playerNumber } = this.props;
+    return (
+      <div>
+        <div className="playerHeading">{playerName}</div>
+        <div
+          className="player"
+          value={playerNumber}
+          defaultValue={playerNumber}
+          onClick={e => this.updateScore(playerNumber, playerName)}
+        ></div>
+      </div>
+    );
+  }
 }
--- src/components/player/player.test.jsx
+++ src/components/player/player.test.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Player from "./player";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 configure({ adapter: new Adapter() });
 
@@ -10,4 +11,13 @@ describe("Player", () => {
     const player = renderer.create(<Player />).toJSON();
     expect(player).toMatchSnapshot();
   });
+  
+  it('handle players ball call "updateScore" prop on button click', () => {
+    const updateScore = jest.fn();
+    const button = shallow((<Player updateScore={updateScore} />));
+    button.find('.player').props().onClick();
+    expect(updateScore).toHaveBeenCalled();
+    expect(updateScore).toHaveBeenCalledTimes(1);
+  });
+  
 });
