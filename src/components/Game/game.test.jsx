88f507d9ef34c080cff2c3f3c6565502d806ac88
2020-DEV-006 <2020dev006@gmail.com>
2019-11-24 23:46:53 +0530

    Handle player win

--- src/components/Game/game.test.jsx
+++ src/components/Game/game.test.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Game from "./Game";
 import { configure } from "enzyme";
+import Player from '../player/player';
+import ScoreBoard from '../scoreBoard/scoreBoard';
+import { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import renderer from "react-test-renderer";
 
@@ -17,5 +20,27 @@ describe("Game", () => {
   it("render Game component correctly", () => {
     const GameComponent = renderer.create(<Game />).toJSON();
     expect(GameComponent).toMatchSnapshot();
+  });
+  it("handle the win scenario", () => {
+    const updateScore = jest.fn();
+    const points = [0, 15, 30, 40, "won"];
+    const button = shallow(<Player updateScore={updateScore} />);
+    var score = shallow(<ScoreBoard />);
+    var status = 0;
+    points.map((data, id) => {
+      if (id > 0) {
+        button
+          .find(".player")
+          .props()
+          .onClick();
+        status = points[id];
+      }
+    });
+    status = status == "won" ? 1 : 0;
+    expect(updateScore).toHaveBeenCalledTimes(4);
+    score.setProps({ firstPlayer: status });
+    expect(score.props().children[1].props.children[1].props.children).toEqual(
+      1
+    );
   });
 });
